ae9e88f0c08e39aa523a81515b4a63fb3e35122c
Prateek <bhandariprateek64@gmail.com>
2025-01-12 15:11:31 +0530

    changed URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,8 @@
-export const USER_API_ENDPOINT = 'http://localhost:8000/api/v1/user';
-export const JOB_API_ENDPOINT = 'http://localhost:8000/api/v1/job';
+export const USER_API_ENDPOINT =
+  'https://career-connect-grdh.onrender.com/api/v1/user';
+export const JOB_API_ENDPOINT =
+  'https://career-connect-grdh.onrender.com/api/v1/job';
 export const APPLICATION_API_ENDPOINT =
-  'http://localhost:8000/api/v1/application';
-export const COMPANY_API_ENDPOINT = 'http://localhost:8000/api/v1/company';
+  'https://career-connect-grdh.onrender.com/api/v1/application';
+export const COMPANY_API_ENDPOINT =
+  'https://career-connect-grdh.onrender.com/api/v1/company';
